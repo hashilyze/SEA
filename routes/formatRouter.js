1f86b8ada7d03e09b6eda017dd32aa3e79c0905e
hashilyze <43923425+hashilyze@users.noreply.github.com>
2023-11-09 14:25:31 +0900

    ADD: Format CRUD

--- routes/formatRouter.js
+++ routes/formatRouter.js
@@ -1,18 +1,20 @@
 // Import
 const express = require("express");
+const Format = require("../models/Format");
+const controller = require("../controllers/formatController");
 // Router
 const router = express.Router();
 
 
 // 형식 생성
-router.post("/", (req, res) => res.send("형식 생성"));
+router.post("/", controller.create);
 // 형식 검색
-router.get("/search", (req, res) => res.send("형식 검색"));
+router.get("/search", controller.findAll);
 // 형식 가져오기
-router.get("/:fid", (req, res) => res.send("형식 가져오기"));
+router.get("/:fid", controller.findOne);
 // 형식 정보 수정
-router.put("/:fid", (req, res) => res.send("형식 정보 수정"));
+router.put("/:fid", controller.updateOne);
 // 형식 삭제
-router.delete("/:fid", (req, res) => res.send("형식 삭제"));
+router.delete("/:fid", controller.deleteOne);
 
 module.exports=router;
