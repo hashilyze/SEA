7d9fd0baba6ad29067e7b5ce5a20e998b39fd0eb
hashilyze <hashilyze@gmail.com>
2023-11-10 01:04:50 +0900

    ADD: Basket API

--- routes/basketRouter.js
+++ routes/basketRouter.js
@@ -1,5 +1,7 @@
 // Import
 const express = require("express");
+const Basket = require("../models/Basket");
+const controller = require("../controllers/basketController");
 // Router
 const router = express.Router();
 
@@ -8,10 +10,10 @@ const router = express.Router();
 router.get("/", (req, res) => res.send("장바구니 페이지"));
 
 // 장바구니에 추가
-router.post("/:uid/:pid", (req, res) => res.send("장바구니에 추가"));
+router.post("/:uid/:pid", controller.addOne);
 // 장바구니에서 제거
-router.delete("/:uid/:pid", (req, res) => res.send("장바구니에서 제거"));
+router.delete("/:uid/:pid", controller.removeOne);
 // 장바구니 가져오기
-router.get("/:uid", (req, res) => res.send("장바구니 가져오기"));
+router.get("/:uid", controller.findAll);
 
 module.exports=router;
